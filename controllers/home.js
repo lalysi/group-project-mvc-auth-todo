946bce6751018694c44c3ee5be3fa7d2979409e4
Mustafa Ali <78193284+MustafaAliDS@users.noreply.github.com>
2022-09-04 17:22:33 -0700

    chore: added a random motivational quote to the home page (#20)

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,19 @@
+const axios = require("axios");
 module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
-    }
-}
+  getIndex: async (req, res) => {
+    const randomNum = Math.floor(Math.random() * 111);
+    const response = await axios
+      .get(
+        "https://goquotes-api.herokuapp.com/api/v1/all?type=tag&val=motivational"
+      )
+      .then(function (response) {
+        quote = response.data.quotes[randomNum].text;
+        author = response.data.quotes[randomNum].author;
+      });
+
+    res.render("index.ejs", {
+      quote: quote,
+      author: author,
+    });
+  },
+};
