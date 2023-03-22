f1d5fa15e067b81672258185e733f5d92cdfc1fb
MustafaAliDS <dsalimustafa@gmail.com>
2023-03-21 19:43:38 -0700

    chore: fix quotes api

--- controllers/home.js
+++ controllers/home.js
@@ -3,12 +3,10 @@ module.exports = {
   getIndex: async (req, res) => {
     const randomNum = Math.floor(Math.random() * 111);
     const response = await axios
-      .get(
-        "https://goquotes-api.herokuapp.com/api/v1/all?type=tag&val=motivational"
-      )
+      .get("https://zenquotes.io/api/random")
       .then(function (response) {
-        quote = response.data.quotes[randomNum].text;
-        author = response.data.quotes[randomNum].author;
+        quote = response.data[0].q;
+        author = response.data[0].a;
       });
 
     res.render("index.ejs", {
